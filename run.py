3ccc5d730738a200772dd5ea7b5d99e35f40183d
yuchiii111 <153926062+yuchiii111@users.noreply.github.com>
2024-02-21 15:31:10 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
 from getanswer import GETANSWER
+import pandas as pd
 
-d_GETANS = GETASNWER()
+
+api_key = ''
+url = ''
+input_file_path = ''
+output_file_path = ''
+
+df = pd.read_csv(input_file_path)
+querys = df['query']
+csv_ans = []
+for query in querys:
+	d_GETANS = GETASNWER(query, api_key, url)
+	csv_ans.append(d_GETANS.get_answer())
+
+df['answer'] = csv_ans
+df.to_csv(output_file_path, index=False)
+	
+	
